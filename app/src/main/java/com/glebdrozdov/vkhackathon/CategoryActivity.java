359f3efdc4fd9605235fe90062f280243ef44122
Ilya <ilja.sahipoff@yandex.ru>
2017-10-21 16:39:40 +0300

    Fix

---
+++ app/src/main/java/com/glebdrozdov/vkhackathon/CategoryActivity.java
@@ -0,0 +1,16 @@
+package com.glebdrozdov.vkhackathon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Илья on 21.10.2017.
+ */
+
+public class CategoryActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
