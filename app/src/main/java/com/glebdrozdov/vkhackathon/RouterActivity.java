e76f8a2dbb9f727c9d58d04e356a73ad254e6a92
Ilya <ilja.sahipoff@yandex.ru>
2017-10-25 01:41:04 +0300

    design editions

--- app/src/main/java/com/glebdrozdov/vkhackathon/RouterActivity.java
+++ app/src/main/java/com/glebdrozdov/vkhackathon/RouterActivity.java
@@ -2,37 +2,49 @@ package com.glebdrozdov.vkhackathon;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
 
-public class RouterActivity extends Activity {
+public class RouterActivity extends AppCompatActivity {
     Button button, button3, button4;
-
+    CardView cv1, cv2, cv3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_router);
-        button = (Button) findViewById(R.id.button);
-        button3 = (Button) findViewById(R.id.button3);
-        button4 = (Button) findViewById(R.id.button4);
-    }
-
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.button:
+        cv1 = (CardView)findViewById(R.id.cv1);
+        cv2 = (CardView)findViewById(R.id.cv2);
+        cv3 = (CardView)findViewById(R.id.cv3);
+        cv1.setCardBackgroundColor(Color.parseColor("#FFEBCD"));
+        cv2.setCardBackgroundColor(Color.parseColor("#FFE4C4"));
+        cv3.setCardBackgroundColor(Color.parseColor("#FFDEAD"));
+        cv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 Intent intent = new Intent(RouterActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
-                break;
-            case R.id.button3:
+            }
+        });
+        cv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 Intent intent3 = new Intent(RouterActivity.this, ListRooms.class);
                 startActivity(intent3);
-                break;
-            case R.id.button4:
+            }
+        });
+        cv3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 Intent intent4 = new Intent(RouterActivity.this, RoomsActivity.class);
                 startActivity(intent4);
-                break;
-        }
+            }
+        });
     }
+
+
 }
